ac8652d596f1e9293085695ab152084517bf6439
hanosong <838519800@qq.com>
2023-02-21 18:18:25 +0800

    cars-item的布局

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -1,8 +1,23 @@
 <template>
-    <section class="index-wrapper">
-        <div class="cars-item"></div>
+    <div class="index-wrapper">
+
+        <section class="cars-item">
+            <header>
+                <h4 class="cars-logo">
+                    <img src="../../assets/images/cars-logo.png" alt="" />
+                    <span class="name"></span>
+                </h4>
+                <p class="cars-attr">
+                    新能源汽车 5座
+                </p>
+            </header>
+            <div class="cars-content">
+
+            </div>
+            <footer></footer>
+        </section>
         汽车列表
-    </section>
+    </div>
 </template>
 <script>
 export default {
